83e24df881f138ae07f9aceb678998a807e60f00
Treshch <vladislavtreshcheyko@gmail.com>
2019-07-07 19:17:34 +0300

    All actions for contact are repeated

---
+++ contact.py
@@ -0,0 +1,30 @@
+class Contact:
+
+
+    def __init__(self, april, aday, byear, march, bday, homepage, email_3, email_2, email, fax, work, mobile,
+                 home, address, company, title, nickname, last_name, middle_name, first_name, ayear,
+                 secondary_address, notes, secondary_home):
+        self.april = april
+        self.aday = aday
+        self.byear = byear
+        self.march = march
+        self.bday = bday
+        self.homepage = homepage
+        self.email_3 = email_3
+        self.email_2 = email_2
+        self.email = email
+        self.fax = fax
+        self.work = work
+        self.mobile = mobile
+        self.home = home
+        self.address = address
+        self.company = company
+        self.title = title
+        self.nickname = nickname
+        self.last_name = last_name
+        self.middle_name = middle_name
+        self.first_name = first_name
+        self.ayear = ayear
+        self.secondary_address = secondary_address
+        self.notes = notes
+        self.secondary_home = secondary_home
---
+++ test_add_contact.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+from contact import Contact
+
+
+class TestAddContact(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+    
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.open_create_contact_page(wd)
+        self.create_contact(wd, Contact(first_name="first name", middle_name="middle name", last_name="last name",
+                       nickname="nickname", title="title", company="company", address="address", home="home",
+                       mobile="mobile", work="work", fax="fax", email="email", email_2="email2", email_3="email3",
+                       homepage="homepage", bday='3', march="March", byear="1997", aday='3', april="April", ayear="2016",
+                       secondary_address="secondary address", secondary_home="secondary home", notes="notes"))
+        self.go_back_to_home_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def go_back_to_home_page(self, wd):
+        wd.find_element_by_link_text("home page").click()
+
+    def create_contact(self, wd, contact):
+        # fill contact form
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").send_keys(contact.home)
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("work").send_keys(contact.work)
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("email2").send_keys(contact.email_2)
+        wd.find_element_by_name("email3").send_keys(contact.email_3)
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        wd.find_element_by_name("bday").click()
+        wd.find_element_by_xpath(f"//option[@value='{contact.bday}']").click()
+        wd.find_element_by_name("bmonth").click()
+        wd.find_element_by_xpath(f"//option[@value='{contact.march}']").click()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        wd.find_element_by_name("aday").click()
+        wd.find_element_by_xpath(f"(//option[@value='{contact.aday}'])[2]").click()
+        wd.find_element_by_name("amonth").click()
+        wd.find_element_by_xpath(f"(//option[@value='{contact.april}'])[2]").click()
+        wd.find_element_by_name("ayear").send_keys(contact.ayear)
+        wd.find_element_by_name("new_group").click()
+        wd.find_element_by_xpath("(//option[@value='3'])[3]").click()
+        wd.find_element_by_name("address2").send_keys(contact.secondary_address)
+        wd.find_element_by_name("phone2").send_keys(contact.secondary_home)
+        wd.find_element_by_name("notes").send_keys(contact.notes)
+        # submit contact creation
+        wd.find_element_by_name("submit").click()
+
+    def open_create_contact_page(self, wd):
+        wd.find_element_by_link_text("add new").click()
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, driver):
+        driver.get("http://localhost/addressbook/")
+
+    def is_element_present(self, how, what):
+        try: self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+
+    
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == "__main__":
+    unittest.main()
