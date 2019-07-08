c371d6da95093372b1e982e0d1d4102c9cb05d57
Treshch <vladislavtreshcheyko@gmail.com>
2019-07-08 13:04:19 +0300

    Extract additional class for Group and Contact

---
+++ fixture/contact.py
@@ -0,0 +1,50 @@
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def go_back_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def create(self, contact):
+        wd = self.app.wd
+        self.open_create_contact_page()
+        # fill contact form
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").send_keys(contact.home)
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("work").send_keys(contact.work)
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("email2").send_keys(contact.email_2)
+        wd.find_element_by_name("email3").send_keys(contact.email_3)
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        wd.find_element_by_name("bday").click()
+        wd.find_element_by_xpath(f"//option[@value='{contact.bday}']").click()
+        wd.find_element_by_name("bmonth").click()
+        wd.find_element_by_xpath(f"//option[@value='{contact.bmonth}']").click()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        wd.find_element_by_name("aday").click()
+        wd.find_element_by_xpath(f"(//option[@value='{contact.aday}'])[2]").click()
+        wd.find_element_by_name("amonth").click()
+        wd.find_element_by_xpath(f"(//option[@value='{contact.amonth}'])[2]").click()
+        wd.find_element_by_name("ayear").send_keys(contact.ayear)
+        wd.find_element_by_name("new_group").click()
+        wd.find_element_by_xpath("(//option[@value='3'])[3]").click()
+        wd.find_element_by_name("address2").send_keys(contact.secondary_address)
+        wd.find_element_by_name("phone2").send_keys(contact.secondary_home)
+        wd.find_element_by_name("notes").send_keys(contact.notes)
+        # submit contact creation
+        wd.find_element_by_name("submit").click()
+        self.go_back_to_home_page()
+
+    def open_create_contact_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,9 +12,9 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login("admin", "secret")
-    app.create_contact(Contact(first_name="first name", middle_name="middle name", last_name="last name",
-                   nickname="nickname", title="title", company="company", address="address", home="home",
-                   mobile="mobile", work="work", fax="fax", email="email", email_2="email2", email_3="email3",
-                   homepage="homepage", bday='3', bmonth="March", byear="1997", aday='3', amonth="April", ayear="2016",
-                   secondary_address="secondary address", secondary_home="secondary home", notes="notes"))
+    app.contact.create(Contact(first_name="first name", middle_name="middle name", last_name="last name",
+                               nickname="nickname", title="title", company="company", address="address", home="home",
+                               mobile="mobile", work="work", fax="fax", email="email", email_2="email2", email_3="email3",
+                               homepage="homepage", bday='3', bmonth="March", byear="1997", aday='3', amonth="April", ayear="2016",
+                               secondary_address="secondary address", secondary_home="secondary home", notes="notes"))
     app.session.logout()
