140deb9d0a2655abaf91a3b8bf86e19751931da4
Treshch <vladislavtreshcheyko@gmail.com>
2019-07-08 13:32:01 +0300

    Created general fixture which can be used by several files.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.contact import Contact
-from fixture.application import Application
 
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_contact(app):
     app.session.login("admin", "secret")
     app.contact.create(Contact(first_name="first name", middle_name="middle name", last_name="last name",
-                               nickname="nickname", title="title", company="company", address="address", home="home",
-                               mobile="mobile", work="work", fax="fax", email="email", email_2="email2", email_3="email3",
-                               homepage="homepage", bday='3', bmonth="March", byear="1997", aday='3', amonth="April", ayear="2016",
-                               secondary_address="secondary address", secondary_home="secondary home", notes="notes"))
+                               nickname="nickname", title="title", company="company", address="address",
+                               home="home", mobile="mobile", work="work", fax="fax", email="email", email_2="email2",
+                               email_3="email3", homepage="homepage", bday='3', bmonth="March", byear="1997",
+                               aday='3', amonth="April", ayear="2016", secondary_address="secondary address",
+                               secondary_home="secondary home", notes="notes"))
     app.session.logout()
