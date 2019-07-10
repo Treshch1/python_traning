2bc68a6bae8e2edbf408b3a4b672dc34162aa934
Treshch <vladislavtreshcheyko@gmail.com>
2019-07-10 17:07:00 +0300

    Add strip method

--- model/group.py
+++ model/group.py
@@ -10,10 +10,10 @@ class Group:
         self.id = id
 
     def __repr__(self):
-        return f"{self.id}, {self.name}"
+        return f"{self.id}, {self.name}, {self.header}, {self.footer}"
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id) and self.name.strip() == other.name
 
     def id_or_max(self):
         if self.id:
